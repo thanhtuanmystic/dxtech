9ea0c1fb8d1c278039fcf46fe057f311e6ca10d6
Thanh Tuan <tuanvp2001@gmail.com>
2024-07-03 17:20:59 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,11 +16,13 @@ let htmlcssArrow = document.querySelector(".htmlcss-arrow");
 htmlcssArrow.onclick = function () {
   navLinks.classList.toggle("show1");
 };
-let moreArrow = document.querySelector(".more-arrow");
-moreArrow.onclick = function () {
-  navLinks.classList.toggle("show2");
-};
-let jsArrow = document.querySelector(".js-arrow");
-jsArrow.onclick = function () {
-  navLinks.classList.toggle("show3");
-};
+// Xử lý slide feedback trang chủ
+$(document).ready(function () {
+  $("#nextBtn").click(function () {
+    $("#feedbackCarousel").carousel("next");
+  });
+
+  $("#prevBtn").click(function () {
+    $("#feedbackCarousel").carousel("prev");
+  });
+});
